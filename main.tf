adf0272f3ce2884d6d64eea4c7c2ac2e74cac8bd
Slaweksbud <37973865+Slaweksbud@users.noreply.github.com>
2024-11-24 14:05:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ variable "project" {
   type = string
 }
 
-resource "google_secret_manager_secret" "sbud-secret-k4" {
+resource "google_secret_manager_secret" "sbud6" {
   project   = var.project 
   secret_id = var.zm3
   replication {
