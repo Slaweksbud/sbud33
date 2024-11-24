29894e519c96b00f9165c872af67ac071f137dcb
Slaweksbud <37973865+Slaweksbud@users.noreply.github.com>
2024-11-24 16:14:06 +0100

    Create sbud.tf

---
+++ sbud.tf
@@ -0,0 +1,15 @@
+variable "zm3" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+resource "google_secret_manager_secret" "sbud6" {
+  project   = var.project 
+  secret_id = var.zm3
+  replication {
+    auto {}
+  }
+}
